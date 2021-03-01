e3e3c4adcf0a5174c80c1703633b0eb1644ad7f8
Paul Novaes <n3times@users.noreply.github.com>
2021-02-28 17:14:38 -0800

    [libn59] Add more tests to n_trig_test.

--- n59lib/test/n_trig_test.c
+++ n59lib/test/n_trig_test.c
@@ -3,6 +3,15 @@
 #include "n.h"
 
 #define PI_MANT 3141592653590LL
+#define PI 3.141592653590
+#define N_ELEMS(array)  ( sizeof(array) / sizeof((array)[0]) )
+
+typedef n_t (*fun_t)(n_t, n_trig_t mode, bool *err);
+
+static void t(char *mode_str, n_t n, n_trig_t mode, char *str, n_t (op)(n_t, n_trig_t, bool *)) {
+  n_t res = op(n, mode, NULL);
+  printf("%s %s % 12f => % 014lld% 03d\n", mode_str, str, n_n2d(n), res.mant, res.exp);
+}
 
 int main() {
   // Forensics.
@@ -12,20 +21,28 @@ int main() {
   printf("Forensics asin(acos(atan(tan(cos(sin(9)))))):\n%lld %d => %lld %d\n\n",
      n.mant, n.exp, res.mant, res.exp);
 
-  n_trig_t mode = N_GRAD;
-  for (double i = -800; i < 800; i += 50) {
-    printf("%f sin %e\n", i, n_n2d(n_sin(n_d2n(i, NULL), mode, NULL)));
-    printf("%f cos %e\n", i, n_n2d(n_cos(n_d2n(i, NULL), mode, NULL)));
-    printf("%f tan %e\n", i, n_n2d(n_tan(n_d2n(i, NULL), mode, NULL)));
+  fun_t funs[] = { n_sin, n_cos, n_tan };
+  char *ops[] = { "sin", "cos", "tan" };
+
+  for (int k = 0; k < N_ELEMS(funs); k++) {
+    for (double i = -720; i < 720; i += 45) {
+      t("Deg: ", n_d2n(i, NULL), N_DEG, ops[k], funs[k]);
+    }
+    printf("\n");
   }
 
-  n = n_make(PI_MANT, 0);
-  mode = N_RAD;
-  printf("%lld sin %e\n", n.mant, n_n2d(n_sin(n, mode, NULL)));
-  printf("%lld cos %e\n", n.mant, n_n2d(n_cos(n, mode, NULL)));
-  printf("%lld tan %e\n", n.mant, n_n2d(n_tan(n, mode, NULL)));
-  n = n_make(PI_MANT / 2, 0);
-  printf("%lld sin %e\n", n.mant, n_n2d(n_sin(n, mode, NULL)));
-  printf("%lld cos %e\n", n.mant, n_n2d(n_cos(n, mode, NULL)));
-  printf("%lld tan %e\n", n.mant, n_n2d(n_tan(n, mode, NULL)));
+  for (int k = 0; k < N_ELEMS(funs); k++) {
+    for (double i = -800; i < 800; i += 50) {
+      t("Grad:", n_d2n(i, NULL), N_GRAD, ops[k], funs[k]);
+    }
+    printf("\n");
+  }
+
+  for (int k = 0; k < N_ELEMS(funs); k++) {
+    for (int i = -16; i < 16; i += 1) {
+      double d = (i/2)*PI/2 + (i%2)*PI/4;
+      t("Rad: ", n_d2n(d, NULL), N_RAD, ops[k], funs[k]);
+    }
+    printf("\n");
+  }
 }
