9469cb9b1d1e9b2bd757374bf430db57eede9e3d
Paul Novaes <n3times@users.noreply.github.com>
2021-03-09 10:44:37 -0800

    [libn59] Clean up n_string_n2s_test.

--- libn59/test/n_string_n2s_test.c
+++ libn59/test/n_string_n2s_test.c
@@ -3,6 +3,9 @@
 #include <stdio.h>
 
 int main(void) {
+  // This is a pretty exhaustive test over thousands of combinations of numbers
+  // and formats.
+
   long long mants[] = { 
                          1000000000000LL,
                          9999999999999LL,
@@ -18,23 +21,28 @@ int main(void) {
                          4999999999999LL,
                                      0LL,
                    };
+
   int exps[] = {
       -99, -98, -12, -11, -10, -9, -8, -7, -6, -5, -4, -3, -2, -1,
       0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 98, 99
   };
+
   int fixes[] = { 0, 5, 8, 9 };
+
   n_format_t formats[] = { N_FLOAT, N_SCI, N_ENG };
+  char *format_strs[] = { "FLOAT", "SCI", "ENG" };
 
   for (int i = 0; i < N_ELEMS(mants); i++) {
     printf("==========================================================\n\n");
     printf("mantissa = %lld\n\n", mants[i]);
     for (int j = 0; j < N_ELEMS(formats); j++) {
       for (int k = 0; k < N_ELEMS(fixes); k++) {
-        printf("=== mode %s | fix %d ===\n\n",
-               j == N_FLOAT ? "FLOAT" : j == N_SCI ? "SCI" : "ENG", fixes[k]);
+        printf("=== mode %s | fix %d ===\n\n", format_strs[j], fixes[k]);
         for (int l = 0; l < N_ELEMS(exps); l++) {
           if (mants[i] == 0 && exps[l]) continue;
+
           n_t n = n_make(mants[i], exps[l]);
+
           char str[N_N2S_MAX_SIZE];
           n_err_t err;
           n_n2s(n, fixes[k], formats[j], (char *)&str, &err);
