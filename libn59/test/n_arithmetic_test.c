d0f526df954aefa86e76d5fa8cc73599b2e6d473
Paul Novaes <n3times@users.noreply.github.com>
2021-03-13 05:21:28 -0800

    [libn59] Clean up n_arithmetic_test.

--- libn59/test/n_arithmetic_test.c
+++ libn59/test/n_arithmetic_test.c
@@ -3,44 +3,69 @@
 #include <assert.h>
 #include <stdio.h>
 
-typedef n_t (*fun_t)(n_t, n_t, n_err_t *);
+/* Arithmetic operator. */
+typedef n_t (*opr_t)(n_t, n_t, n_err_t *);
 
-static void p(n_t n1, n_t n2, n_t res, n_err_t err, char *op) {
+/* Prints operation. */
+static void p(n_t n1, n_t n2, n_t res, n_err_t err, char *opr) {
   char s1[N_PRINT_MAX_SIZE];
   char s2[N_PRINT_MAX_SIZE];
   char s_res[N_PRINT_MAX_SIZE];
   printf("%s %s %s   =   %s%s\n",
          n_print(n1, s1),
-         op,
+         opr,
          n_print(n2, s2),
          n_print(res, s_res),
          err ? "?" : "");
 }
 
+static void test_all_operators() {
+  n_err_t err;
+  n_t res;
+
+  n_t numbers[] = { TEST_NUMBERS };
+  opr_t oprs[] = { n_plus, n_minus, n_times, n_div, n_pow, n_ipow };
+  char *opr_strs[] = { "+", "-", "*", "/", "^", "v" };
+
+  for (int i = 0; i < N_ELEMS(oprs); i++) {
+    for (int j = 0; j < N_ELEMS(numbers); j++) {
+      for (int k = 0; k < N_ELEMS(numbers); k++) {
+        res = oprs[i](numbers[j], numbers[k], &err);
+        p(numbers[j], numbers[k], res, err, opr_strs[i]);
+      }
+      printf("\n");
+    }
+    printf("\n\n\n");
+  }
+}
+
 static void test_multiplication() {
-  // We truncate result to 13 digits with no rounding. Same as TI-59.
-  n_t n1 = n_make(1111111111111LL, 0);
-  n_t n2 = n_d2n(99, NULL);
-  n_t res = n_times(n1, n2, NULL);
+  n_t n1, n2, res;
+
+  // Multiplication truncates result to 13 digits with no rounding. Same as
+  // TI-59.
+  n1 = n_make(1111111111111LL, 0);
+  n2 = n_d2n(99, NULL);
+  res = n_times(n1, n2, NULL);
   assert(n_equals(res, n_make(1099999999999LL, 2)));  // And not 101.
   p(n1, n2, res, N_ERR_NONE, "*");
 
-  // We always give 13 accurate digits unlike TI-59.
+  // Multiplication always gives 13 accurate digits, unlike TI-59.
   // TI-59 gets (9999999999989, 1) while we get (9999999999998, 1).
   n1 = n_make(9999999999999LL, 0);
   res = n_times(n1, n1, NULL);
   p(n1, n1, res, N_ERR_NONE, "*");
 
-  // Same accuracy as TI-59 in mantissas with less than 13 significant digits:
-  // pi^100 using multiplication. Note that pi = (3141592653590, 0), its
-  // mantissa ending in 0.
+  // Same accuracy as TI-59 for mantissas with less than 13 significant digits:
+  // pi^100 using multiplication. Note that pi = (3141592653590, 0) has 12
+  // significant digits, its mantissa ending in 0.
   res = N_1;
   for (int i = 0; i < 100; i++) {
     res = n_times(res, N_PI, NULL);
   }
   p(N_PI, N_PI, res, N_ERR_NONE, "?");
 
-  // Same accuracy as TI-59 in mantissas with less than 13 significant digits:
+  // Same accuracy as TI-59 for mantissas with less than 13 significant digits:
   // 69! (See Pgm 16, from Master Library).
   res = N_1;
   for (int i = 69; i >= 1; i--) {
@@ -51,21 +76,23 @@ static void test_multiplication() {
 }
 
 static void test_addition() {
-  // Truncation before addition, as in TI-59.
-  n_t n1 = n_make(9999999999999LL, 13);
-  n_t n2 = n_make(9999999999999LL, 0);
-  n_t res = n_plus(n1, n2, NULL);
+  n_t n1, n2, res;
+
+  // Number truncation before addition. Same as TI-59.
+  n1 = n_make(9999999999999LL, 13);
+  n2 = n_make(9999999999999LL, 0);
+  res = n_plus(n1, n2, NULL);
   assert(n_equals(res, n1));  // Note that n2 is ignored
   p(n1, n2, res, N_ERR_NONE, "+");
 
-  // Truncation before substraction, as in TI-59.
+  // Number truncation before substraction. Same as TI-59.
   n1 = n_make(1000000000000LL, 13);
   n2 = N_1;
   res = n_minus(n1, n2, NULL);
   assert(n_equals(res, n1));  // Note that n2 is ignored
   p(n1, n2, res, N_ERR_NONE, "-");
 
-  // TI-59 accurate addition: pi*100 using addition.
+  // Addition has same accuracy as TI-59: pi*100 using addition.
   res = N_0;
   for (int i = 0; i < 100; i++) {
     res = n_plus(res, N_PI, NULL);
@@ -74,25 +101,7 @@ static void test_addition() {
 }
 
 int main() {
-  n_err_t err;
-  n_t res;
-  n_t ns[] = { TEST_NUMBERS };
-
-  fun_t oprs[] = { n_plus, n_minus, n_times, n_div, n_pow, n_ipow };
-  char *opr_strs[] = { "+", "-", "*", "/", "^", "v" };
-
-  // Test all the arithmetic operators for all the test number combinations.
-  for (int i = 0; i < N_ELEMS(oprs); i++) {
-    for (int j = 0; j < N_ELEMS(ns); j++) {
-      for (int k = 0; k < N_ELEMS(ns); k++) {
-        res = oprs[i](ns[j], ns[k], &err);
-        p(ns[j], ns[k], res, err, opr_strs[i]);
-      }
-      printf("\n");
-    }
-    printf("\n\n\n");
-  }
-
+  test_all_operators();
   test_addition();
   test_multiplication();
 }
