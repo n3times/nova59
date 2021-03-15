0f3b27b977f04c1fdffc7f46b7977eb9185d7475
Paul Novaes <n3times@users.noreply.github.com>
2021-03-14 20:52:55 -0700

    [libn59] Clean up n_math_test.c.

--- libn59/test/n_math_test.c
+++ libn59/test/n_math_test.c
@@ -3,13 +3,11 @@
 #include <math.h>
 #include <stdio.h>
 
-typedef n_t (*fun_t)(n_t n, n_err_t *err);
-typedef n_t (*fun2_t)(n_t n);
-
 static void p(n_t n, n_t res, n_err_t err, char *op) {
   char s[N_PRINT_MAX_SIZE];
   char s_res[N_PRINT_MAX_SIZE];
-  printf("%s %s   =   %s%s\n",
+
+  printf("%s %s    =   %s%s\n",
          n_print(n, s),
          op,
          n_print(res, s_res),
@@ -17,12 +15,14 @@ static void p(n_t n, n_t res, n_err_t err, char *op) {
 }
 
 static void test_funs() {
+  typedef n_t (*fun_t)(n_t n, n_err_t *err);
+
   n_err_t err;
   n_t res;
   n_t ns[] = { TEST_NUMBERS_2 };
-
   fun_t funs[] = { n_square, n_sqrt, n_log, n_ln, n_1_x, n_exp, n_pow10 };
-  char *fun_strs[] = { "square", "sqrt", "log", "ln", "1/x" , "exp", "pow10" };
+  char *fun_strs[] =
+      { "  ^2", "  vx", " log", "  ln", " 1/x" , " exp", " ^10" };
 
   // Test all the functions for all the test numbers.
   for (int i = 0; i < N_ELEMS(funs); i++) {
@@ -30,16 +30,17 @@ static void test_funs() {
       res = funs[i](ns[j], &err);
       p(ns[j], res, err, fun_strs[i]);
     }
-    printf("\n\n\n");
+    printf("\n\n");
   }
 }
 
 static void test_funs_2() {
+  typedef n_t (*fun_t)(n_t n);
+
   n_t res;
   n_t ns[] = { TEST_NUMBERS_2 };
-
-  fun2_t funs[] = { n_int, n_frac, n_abs, n_chs };
-  char *ops[] = { "int", "frac", "abs", "chs" };
+  fun_t funs[] = { n_int, n_frac, n_abs, n_chs };
+  char *ops[] = { " int", " frc", " abs", " chs" };
 
   // Test all the functions for all the test numbers.
   for (int i = 0; i < N_ELEMS(funs); i++) {
@@ -47,28 +48,36 @@ static void test_funs_2() {
       res = funs[i](ns[j]);
       p(ns[j], res, N_ERR_NONE, ops[i]);
     }
-    printf("\n\n\n");
+    printf("\n\n");
   }
 }
+
 static void test_int() {
   n_t res;
 
   for (int i = -1; i < 14; i++) {
     res = n_make(pow(10, i));
-    p(res, n_int(res), N_ERR_NONE, "int");
+    p(res, n_int(res), N_ERR_NONE, " int");
   }
+  printf("\n");
+
   for (int i = -2; i < 14; i++) {
     res = n_make(9.999999999999 * pow(10, i));
-    p(res, n_int(res), N_ERR_NONE, "int");
+    p(res, n_int(res), N_ERR_NONE, " int");
   }
+  printf("\n");
+
   for (int i = -1; i < 14; i++) {
     res = n_make(1.234567890123 * pow(10, i));
-    p(res, n_int(res), N_ERR_NONE, "int");
+    p(res, n_int(res), N_ERR_NONE, " int");
   }
+  printf("\n");
+
   for (int i = -1; i < 14; i++) {
     res = n_make(-1.234567890123 * pow(10, i));
-    p(res, n_int(res), N_ERR_NONE, "int");
+    p(res, n_int(res), N_ERR_NONE, " int");
   }
+  printf("\n");
 }
 
 static void test_frac() {
@@ -76,19 +85,25 @@ static void test_frac() {
 
   for (int i = -1; i < 14; i++) {
     res = n_make(pow(10, i));
-    p(res, n_frac(res), N_ERR_NONE, "frac");
+    p(res, n_frac(res), N_ERR_NONE, " frc");
   }
+  printf("\n");
+
   for (int i = -2; i < 14; i++) {
     res = n_make(9.999999999999 * pow(10, i));
-    p(res, n_frac(res), N_ERR_NONE, "frac");
+    p(res, n_frac(res), N_ERR_NONE, " frc");
   }
+  printf("\n");
+
   for (int i = -1; i < 14; i++) {
     res = n_make(1.234567890123 * pow(10, i));
-    p(res, n_frac(res), N_ERR_NONE, "frac");
+    p(res, n_frac(res), N_ERR_NONE, " frc");
   }
+  printf("\n");
+
   for (int i = -1; i < 14; i++) {
     res = n_make(-1.234567890123 * pow(10, i));
-    p(res, n_frac(res), N_ERR_NONE, "frac");
+    p(res, n_frac(res), N_ERR_NONE, " frc");
   }
 }
 
@@ -98,8 +113,6 @@ int main() {
   test_funs_2();
 
   test_int();
-
-  printf("\n");
 
   test_frac();
 }
